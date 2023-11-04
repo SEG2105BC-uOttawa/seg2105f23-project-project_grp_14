68f42041328fe381c04eceb4b8a07148b8c13965
FayzaAlicia <144732969+FayzaAlicia@users.noreply.github.com>
2023-11-04 14:19:57 -0400

    Update LoginAdminSuccess.java

--- app/src/main/java/com/example/gcc_application/LoginAdminSuccess.java
+++ app/src/main/java/com/example/gcc_application/LoginAdminSuccess.java
@@ -2,13 +2,33 @@ package com.example.gcc_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginAdminSuccess extends AppCompatActivity {
 
+    TextView welcomeTextView;
+    DatabaseHelper databaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_admin_success);
+
+        welcomeTextView = findViewById(R.id.welcomeTextView);
+        databaseHelper = new DatabaseHelper(this);
+
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("USERNAME");
+        String role = intent.getStringExtra("ROLE");
+
+        welcomeTextView.setText("Welcome, " + username + "!\nRole: " + role);
+
+    }
+
+    public void eventManagementBtnClick(View view){
+        Intent myIntent = new Intent(LoginAdminSuccess.this, AdminEventManagement.class);
+        startActivity(myIntent);
     }
 }
