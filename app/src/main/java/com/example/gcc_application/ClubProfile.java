cdeda7f3f891c93e8a6103022b3ec8f67bbd0ff3
FayzaAlicia <144732969+FayzaAlicia@users.noreply.github.com>
2023-12-05 04:02:00 -0500

    Fixed Conflict Error

--- app/src/main/java/com/example/gcc_application/ClubProfile.java
+++ app/src/main/java/com/example/gcc_application/ClubProfile.java
@@ -58,15 +58,34 @@ public class ClubProfile extends AppCompatActivity {
     }
 
     // Getters and setters for existing fields
-    public String getInstagramLink() { return instagramLink; }
-    public void setInstagramLink(String instagramLink) { this.instagramLink = instagramLink; }
-    public String getContactName() { return contactName; }
-    public void setContactName(String contactName) { this.contactName = contactName; }
-    public String getPhoneNumber() { return phoneNumber; }
-    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public String getInstagramLink() {
+        return instagramLink;
+    }
+
+    public void setInstagramLink(String instagramLink) {
+        this.instagramLink = instagramLink;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
     // Getters for the ratings
-    public float getAverageRating() { return averageRating; }
+    public float getAverageRating() {
+        return averageRating;
+    }
 
     public void confirmBtn(View view) {
         String rated = rating.getText().toString().trim();
@@ -76,14 +95,15 @@ public class ClubProfile extends AppCompatActivity {
         String clubName = intent.getStringExtra("clubName");
 
 
-        if (commented.isEmpty() || commented.isEmpty()){
+        if (commented.isEmpty() || commented.isEmpty()) {
             Toast.makeText(ClubProfile.this, "Please fill a fields", Toast.LENGTH_SHORT).show();
         } else if (Integer.parseInt(rated) > 5 || (Integer.parseInt(rated) < 0)) {
             Toast.makeText(ClubProfile.this, "Invalid rating", Toast.LENGTH_SHORT).show();
-        }else{
+        } else {
             Toast.makeText(ClubProfile.this, "Review Registered", Toast.LENGTH_SHORT).show();
             Rating review = new Rating(Integer.parseInt(rated), commented, clubName);
             addRating(review);
         }
 
     }
+}
